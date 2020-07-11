a8a6ff30541575db282bed5f5bd52e372c6c3de8
Jarquevious <jarquevious@gmail.com>
2020-07-11 11:46:28 -0700

    first commit

---
+++ utils.js
@@ -0,0 +1,64 @@
+// ========================================================
+// Level 1 Challenges
+// ========================================================
+
+const sayHello = () => {
+  return "Hello"
+}
+
+const area = (w, h) => {
+  // should return the area
+  
+  return w * h
+}
+
+const perimeter = (w, h) => {
+  // should return the perimeter 
+  return (w * h)/2
+}
+
+const circleArea = r => {
+  // should return the area of the circle
+  PI = 3.142
+  return PI * (r * r)
+}
+
+
+// ========================================================
+// Level 2 Challenges
+// ========================================================
+// NOTE: You will need to implement methods below (not yet
+// defined) in order to make the tests pass.
+// ========================================================
+
+const shoppingCart = []
+
+const clearCart = () => {
+  shoppingCart.length = 0
+}
+
+const createItem = (name, price) => {
+  return { name, price, quantity: 1 }
+}
+
+const getShoppingCart = () => {
+  // should return the current state of shopping cart
+}
+
+const addItemToCart = (item) => {
+  // should add item to shopping cart
+}
+
+const getNumItemsInCart = () => {
+  // should return the total quantity of items in cart
+}
+
+const removeItemFromCart = (item) => {
+  // should remove item from shopping cart
+}
+
+module.exports = {
+  sayHello, area, perimeter, circleArea,
+  clearCart, createItem, getShoppingCart, addItemToCart,
+  getNumItemsInCart, removeItemFromCart
+}
